ed0690fcf259cc79755f15a8d53fc56cb91ece7e
daniel.donaldson <dandon08@gmail.com>
2023-08-16 17:04:01 +0200

    test site verification

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,7 @@ async fn handler_healthy() -> impl IntoResponse {
 struct CaptchaForm {
     #[serde(rename = "g-recaptcha-response")]
     g_recaptcha_response: String,
+    site: String,
     #[serde(flatten)]
     pub fields_in_contact_form: HashMap<String, Value>,
 }
@@ -64,10 +65,18 @@ async fn handler_captcha(Form(form): Form<CaptchaForm>) -> impl IntoResponse {
             if json["success"].as_bool().unwrap_or(false) {
                 // continue on to do actions (i.e. send mail to info box)
                 println!("Success for token {}", &form.g_recaptcha_response);
-                Response::builder()
-                    .status(StatusCode::OK)
-                    .body(json!({"message": "Captcha verification successful"}).to_string())
-                    .unwrap()
+                // send email
+                if send_email_based_on_site(&form.site).is_ok() {
+                    Response::builder()
+                        .status(StatusCode::OK)
+                        .body(json!({"message": "Captcha verification successful"}).to_string())
+                        .unwrap()
+                } else {
+                    Response::builder()
+                        .status(StatusCode::UNAUTHORIZED)
+                        .body(json!({"message": "Site not found"}).to_string())
+                        .unwrap()
+                }
             } else {
                 // If Error send back generic failed error
                 println!("Error for token {}. ", &form.g_recaptcha_response);
@@ -86,5 +95,13 @@ async fn handler_captcha(Form(form): Form<CaptchaForm>) -> impl IntoResponse {
                 .body(json!({"message": "Captcha verification failed"}).to_string())
                 .unwrap()
         }
+    }
+}
+
+fn send_email_based_on_site(site: &str) -> Result<()> {
+    if env::var(format!("{}_SMTP_SERVER", site.to_ascii_uppercase())).is_ok() {
+        Ok(())
+    } else {
+        Err(Error::SiteNotFoundError)
     }
 }
